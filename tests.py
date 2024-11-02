24223a352725cd344877f4793102088e38f10be0
Matt Norton <matt@carrotmanmatt.com>
2024-11-02 02:00:33 +0000

    Improve package

--- tests.py
+++ tests.py
@@ -1,107 +1,125 @@
-import unittest
-from classproperties import *
+"""Full test suite for 'classproperties' library."""
+
+import abc
+from typing import TYPE_CHECKING, Any
+
+from classproperties import cached_classproperty, classproperty
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+__all__: "Sequence[str]" = ("TestClassProperty", "TestCachedClassProperty")
 
 
-def get_cls_definition():
-    class Cls(object):
+class BaseTestClassProperty(abc.ABC):  # noqa: B024
+    """Abstract base class for all test classes."""
 
-        @classproperty
-        def prop(cls):
-            return 1000
+    @classmethod
+    def _get_cls_definition(cls, test_value: object) -> Any:  # noqa: ANN401
+        class _HolderClass:
+            cached_prop_exec_count = 0
 
-        cached_prop_exec_count = 0
+            @classproperty
+            def held_value(cls) -> object:
+                return test_value
 
-        @cached_classproperty
-        def cached_prop(cls):
-            cls.cached_prop_exec_count += 1
-            return 2000
-    return Cls
+            @cached_classproperty
+            def cached_held_value(cls):  # noqa: ANN202
+                cls.cached_prop_exec_count += 1
+                return test_value
 
-
-class TestClassProperty(unittest.TestCase):
-    """This class contains all tests of the library"""
-      
-    def test_classproperty(self):
-        Cls = get_cls_definition()
-        self.assertEqual(Cls.prop, 1000)
-
-    def test_classproperty_instance(self):
-        Cls = get_cls_definition()
-        self.assertEqual(Cls().prop, 1000)
+        return _HolderClass
 
 
-class TestCachedClassProperty(unittest.TestCase):
-    def test_cached_classproperty(self):
-        Cls = get_cls_definition()
-        self.assertEqual(Cls.cached_prop_exec_count, 0)
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
+class TestClassProperty(BaseTestClassProperty):
+    """Test the `classproperty` decorator."""
 
-    def test_cached_classproperty_executed_once(self):
-        Cls = get_cls_definition()
-        self.assertEqual(Cls.cached_prop_exec_count, 0)
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
+    def test_classproperty_class(self) -> None:
+        """Test that a classproperty correctly returns its value from a class."""
+        assert self._get_cls_definition(1000).held_value == 1000
 
-    def test_cached_classproperty_delete_cache(self):
-        Cls = get_cls_definition()
-        self.assertEqual(Cls.cached_prop_exec_count, 0)
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
-        cached_classproperty.remove_cached_value(Cls, 'cached_prop')
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 2)
-
-    def test_cached_classproperty_instance(self):
-        Cls = get_cls_definition()
-        inst = Cls()
-        self.assertEqual(inst.cached_prop_exec_count, 0)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-
-    def test_cached_classproperty_instance_with_deletion(self):
-        Cls = get_cls_definition()
-        inst = Cls()
-        self.assertEqual(inst.cached_prop_exec_count, 0)
-        self.assertEqual(inst.cached_prop, 2000)
-
-        inst.cached_prop = 3000
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop, 3000)
-
-        del inst.cached_prop
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
-
-    def test_cached_classproperty_executed_once_instance(self):
-        Cls = get_cls_definition()
-        inst = Cls()
-        self.assertEqual(inst.cached_prop_exec_count, 0)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-
-    def test_cached_classproperty_executed_once_instance_cls_share_same_cached_val(self):
-        Cls = get_cls_definition()
-        inst = Cls()
-        self.assertEqual(inst.cached_prop_exec_count, 0)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
-        self.assertEqual(Cls.cached_prop, 2000)
-        self.assertEqual(Cls.cached_prop_exec_count, 1)
-
-        inst = Cls()
-        self.assertEqual(inst.cached_prop_exec_count, 1)
-        self.assertEqual(inst.cached_prop, 2000)
-        self.assertEqual(inst.cached_prop_exec_count, 1)
+    def test_classproperty_instance(self) -> None:
+        """Test that a classproperty correctly returns its value from an instance."""
+        assert self._get_cls_definition(2000)().held_value == 2000
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestCachedClassProperty(BaseTestClassProperty):
+    """Test the `cached_classproperty` decorator."""
+
+    def test_cached_classproperty_class(self) -> None:
+        """Test that a `cached_classproperty` correctly returns its value from a class."""
+        created_class: Any = self._get_cls_definition(3000)
+        assert created_class.cached_prop_exec_count == 0
+        assert created_class.cached_held_value == 3000
+        assert created_class.cached_prop_exec_count == 1
+
+    def test_cached_classproperty_executed_once(self) -> None:
+        """Test that a `cached_classproperty` correctly returns the cached value."""
+        created_class: Any = self._get_cls_definition(4000)
+        assert created_class.cached_prop_exec_count == 0
+        assert created_class.cached_held_value == 4000
+        assert created_class.cached_prop_exec_count == 1
+        assert created_class.cached_held_value == 4000
+        assert created_class.cached_prop_exec_count == 1
+
+    def test_cached_classproperty_delete_cache(self) -> None:
+        """Test that removing a `cached_classproperty`'s cached value functions correctly."""
+        created_class: Any = self._get_cls_definition(5000)
+        assert created_class.cached_prop_exec_count == 0
+        assert created_class.cached_held_value == 5000
+        assert created_class.cached_prop_exec_count == 1
+        cached_classproperty.remove_cached_value(created_class, "cached_held_value")
+        assert created_class.cached_held_value == 5000
+        assert created_class.cached_prop_exec_count == 2
+
+    def test_cached_classproperty_instance(self) -> None:
+        """Test that a `cached_classproperty` returns the correct value from an instance."""
+        created_instance: Any = self._get_cls_definition(6000)()
+        assert created_instance.cached_prop_exec_count == 0
+        assert created_instance.cached_held_value == 6000
+        assert created_instance.cached_prop_exec_count == 1
+
+    def test_cached_classproperty_instance_with_deletion(self) -> None:
+        """Test that a `cached_classproperty` can delete its cached value from an instance."""
+        created_class: Any = self._get_cls_definition(7000)
+        created_instance: Any = created_class()
+        assert created_instance.cached_prop_exec_count == 0
+        assert created_instance.cached_held_value == 7000
+
+        created_instance.cached_held_value = 750
+        assert created_class.cached_held_value == 7000
+        assert created_instance.cached_held_value == 750
+
+        del created_instance.cached_held_value
+        assert created_class.cached_held_value == 7000
+        assert created_class.cached_prop_exec_count == 1
+
+    def test_cached_classproperty_executed_once_instance(self) -> None:
+        """Test that a `cached_classproperty` returns its value from an instance only once."""
+        created_instance: Any = self._get_cls_definition(8000)()
+        assert created_instance.cached_prop_exec_count == 0
+        assert created_instance.cached_held_value == 8000
+        assert created_instance.cached_prop_exec_count == 1
+        assert created_instance.cached_held_value == 8000
+        assert created_instance.cached_prop_exec_count == 1
+
+    def test_cached_classproperty_executed_once_instance_cls_share_same_cached_val(
+        self,
+    ) -> None:
+        """Test that a `cached_classproperty` returns its value from a shared class once."""
+        created_class: Any = self._get_cls_definition(9000)
+        created_instance: Any = created_class()
+        assert created_instance.cached_prop_exec_count == 0
+        assert created_instance.cached_held_value == 9000
+        assert created_instance.cached_prop_exec_count == 1
+        assert created_instance.cached_held_value == 9000
+        assert created_instance.cached_prop_exec_count == 1
+
+        assert created_class.cached_prop_exec_count == 1
+        assert created_class.cached_held_value == 9000
+        assert created_class.cached_prop_exec_count == 1
+
+        created_instance = created_class()
+        assert created_instance.cached_prop_exec_count == 1
+        assert created_instance.cached_held_value == 9000
+        assert created_instance.cached_prop_exec_count == 1
